3085965f97efcf1c53aa126423bde979bb0d74e3
alex <avtor_house@mail.ru>
2021-07-10 12:58:00 +0300

    version 2

---
+++ src/release_2/V2_1__kvit_file_pkg_body.sql
@@ -0,0 +1,13 @@
+﻿create or replace package body kvit_file_pkg is
+
+  -- Author  : ALEX
+  -- Created : 10.07.2021 11:15:32
+  -- Purpose : Работа с файлами
+
+  procedure test is
+  begin
+    dbms_output.put_line('test procedure');
+  end;
+  
+end kvit_file_pkg;
+/
